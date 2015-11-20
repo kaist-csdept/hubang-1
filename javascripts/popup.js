0f2fe726d172a71bb2821fc5e3dc77fd3198295b
minsoo10000 <minsoo10000@kaist.ac.kr>
2015-11-20 15:51:49 +0900

    popup들 js파일 생성
    toggle 위치 수정 멘트 등록

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,9 +1,11 @@
 chrome.storage.local.get("power", function (s) {
   if (s.power) {
     $("#power").addClass("on");
+	$("#plugin_ment").text("후방주의 플러그인이 현재 작동중입니다.");
   }
   else {
     $("#power").addClass("off");
+	$("#plugin_ment").text("후방주의 플러그인이 현재 비활성화되어 있습니다.");
   }
 });
 
@@ -18,12 +20,14 @@ $("#power").click(function () {
     $(this).addClass("off");
     $(this).removeClass("on");
     chrome.storage.local.set({"power": false});
+	$("#plugin_ment").text("후방주의 플러그인이 현재 비활성화되어 있습니다.");
     refresh();
   }
   else {
     $(this).addClass("on");
     $(this).removeClass("off");
     chrome.storage.local.set({"power": true});
+	$("#plugin_ment").text("후방주의 플러그인이 현재 작동중입니다.");
     refresh();
   }
 });
---
+++ javascripts/popup1.js
@@ -0,0 +1,38 @@
+﻿chrome.storage.local.get("level", function (t) {
+  if (t.level==1){
+	$("#textlog").text("현재 설정은 낮음입니다.");
+  }
+  else if (t.level==2){
+	$("#textlog").text("현재 설정은 낮음입니다.");
+  }
+  else if (t.level==3){
+	$("#textlog").text("현재 설정은 높음입니다.");
+  }
+  else if (t.level==4){
+	$("#textlog").text("현재 설정은 매우 높음입니다.");
+  }
+  else{
+	$("#textlog").text("현재 설정은 매우 낮음입니다.");
+  }
+});
+
+/*$(function() {
+  $('#test').on('change',function() {
+    num=int $(this).val;
+    if(num==1){
+	  $('#log').text("현재 레벨 : 낮음");
+	}
+	else if(num==2){
+	  $('#log').text("현재 레벨 : 보통");
+	}
+	else if(num==3){
+	  $('#log').text("현재 레벨 : 높음");
+	}
+	else if(num==4){
+	  $('#log').text("현재 레벨 : 매우 높음");
+	}
+	else{
+	  $('#log').text("현재 레벨 : 매우 낮음");
+	}
+  });
+});*/
