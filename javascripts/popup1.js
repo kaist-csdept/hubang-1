f8606603f19056bb022d39030863633dbb9e6842
minsoo10000 <minsoo10000@kaist.ac.kr>
2015-11-20 17:07:58 +0900

    popup1 수정

--- javascripts/popup1.js
+++ javascripts/popup1.js
@@ -1,24 +1,34 @@
 ﻿chrome.storage.local.get("level", function (t) {
   if (t.level==1){
 	$("#textlog").text("현재 설정은 낮음입니다.");
+	$('#log').text("현재 레벨 : 낮음");
+	document.getElementById("test").value=1;
   }
   else if (t.level==2){
 	$("#textlog").text("현재 설정은 낮음입니다.");
+	$('#log').text("현재 레벨 : 보통");
+	document.getElementById("test").value=2;
   }
   else if (t.level==3){
 	$("#textlog").text("현재 설정은 높음입니다.");
+	$('#log').text("현재 레벨 : 높음");
+	document.getElementById("test").value=3;
   }
   else if (t.level==4){
 	$("#textlog").text("현재 설정은 매우 높음입니다.");
+	$('#log').text("현재 레벨 : 매우 높음");
+	document.getElementById("test").value=4;
   }
   else{
 	$("#textlog").text("현재 설정은 매우 낮음입니다.");
+	$('#log').text("현재 레벨 : 매우 낮음");
+	document.getElementById("test").value=0;
   }
 });
 
-/*$(function() {
+$(function() {
   $('#test').on('change',function() {
-    num=int $(this).val;
+    num=document.getElementById("test").value;
     if(num==1){
 	  $('#log').text("현재 레벨 : 낮음");
 	}
@@ -35,4 +45,4 @@
 	  $('#log').text("현재 레벨 : 매우 낮음");
 	}
   });
-});*/
+});
