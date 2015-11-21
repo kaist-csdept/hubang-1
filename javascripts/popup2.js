ae467e9f27364de2b4ae2f0128b60d604a1bd47e
minsoo10000 <minsoo10000@kaist.ac.kr>
2015-11-22 04:00:26 +0900

    level 저장부분 해결과 이미지 몇 개 제거

---
+++ javascripts/popup2.js
@@ -0,0 +1,5 @@
+﻿chrome.storage.local.get("tlevel", function (u) {
+  chrome.storage.local.set({"level": u.tlevel});
+  chrome.storage.local.set({"preset": false});
+  location.href='popup.html';
+});
