9bcc423b08849e78cb9874bcdb6738ce649aaa37
Hankook Lee <gksrnr0202@gmail.com>
2015-11-25 23:53:01 +0900

    main.js에서 image 사용할 수 있도록 변경

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,20 +5,40 @@ chrome.storage.local.get("power", function (s) {
   run(window);
 });
 
+function check(data, width, height) {
+  // (r, g, b, ?) = (data[4i], data[4i+1], data[4i+2], data[4i+3])
+  return true;
+}
+
+function checkAndHide(imgElement) {
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+
+  var image = new Image;
+  image.crossOrigin = "Anonymous";
+  image.onload = function () {
+    canvas.width = image.width;
+    canvas.height = image.height;
+    ctx.drawImage(image, 0, 0);
+    var imageData = ctx.getImageData(1, 0, canvas.width, canvas.height).data;
+    if (check(imageData, canvas.width, canvas.height))
+      $(imgElement).hide();
+  };
+  image.src = imgElement.src;
+}
+
 function run(win) {
   if (!win) return;
   var doc = win.document;
   if (!power) return;
 
   $("img").each(function () {
-    $(this).hide();
+    checkAndHide(this);
   });
 
   $("iframe").ready(function () {
-    console.log(this);
-    console.log($(this).contents().find("img"))
     $(this).contents().find("img").each(function () {
-      $(this).hide();
+      checkAndHide(this);
     });
   });
 
@@ -27,7 +47,7 @@ function run(win) {
       var newNodes = m.addedNodes;
       if (newNodes !== null) {
         $(newNodes).find("img").each(function () {
-          $(this).hide();
+          checkAndHide(this);
         });
       }
     });
